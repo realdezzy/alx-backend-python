f5e1c2d220dacced6b5ca4177cb07835bb4b0cb7
realdezzy <realdezzy1@gmail.com>
2023-07-06 14:49:25 +0100

    whitespace

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,16 +2,20 @@
 """
     Safely get value
 """
-from typing import Mapping, Union,TypeVar, Any
+from typing import Mapping, Union, TypeVar, Any
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar, None]= None) -> Union[Any, TypeVar]:
+def safely_get_value(
+        dct: Mapping,
+        key: Any,
+        default: Union[TypeVar, None] = None) -> Union[Any, TypeVar]:
     """Safely get value
 
     Args:
         dct (Mapping): safe dict
         key (Any): any type
-        default (Union[TypeVar, None], optional): default value. Defaults to None.
+        default (Union[TypeVar, None], optional): default value.
+            Defaults to None.
 
     Returns:
         Union[Any, TypeVar]: any
