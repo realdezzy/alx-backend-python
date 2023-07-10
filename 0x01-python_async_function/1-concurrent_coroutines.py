4c0ab3d751e1bc604c79e2e899daa485d5e4f2e5
realdezzy <realdezzy1@gmail.com>
2023-07-10 13:11:39 +0100

    Measure time

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,11 +3,11 @@
     Concurrent coroutines
 """
 import asyncio
-
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Calls wait_random n times and returns delay
 
@@ -18,6 +18,5 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
     Returns:
         list[float]: list of all delay values
     """
-    delay_array: list[float] = [await wait_random(max_delay) for _ in range(n)]
-
-    return sorted(delay_array)
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,6 +2,7 @@
 """
 Measure time
 """
+import asyncio
 import time
 wait_n = __import__("1-concurrent_coroutines").wait_n
 
@@ -17,3 +18,8 @@ def measure_time(n: int, max_delay: int) -> float:
     Returns:
         float: Average time
     """
+    start = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.perf_counter() - start
+
+    return end / n
