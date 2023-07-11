96ef7fc2c3f245884317caf8c818d7abea9e3b67
realdezzy <realdezzy1@gmail.com>
2023-07-11 19:31:32 +0100

    Task 1 - 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""
+Measure Time
+"""
+import asyncio
+from time import perf_counter
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Measure the runtime for four coroutines
+
+    Returns:
+        float: Total runtime
+    """
+    async_comps = [async_comprehension(), async_comprehension(),
+                   async_comprehension(), async_comprehension()]
+    start = perf_counter()
+    await asyncio.gather(*async_comps)
+    time_taken = perf_counter() - start
+
+    return time_taken
--- 0x02-python_async_comprehension/test.py
+++ 0x02-python_async_comprehension/test.py
@@ -2,12 +2,13 @@
 
 import asyncio
 
-async_generator = __import__('0-async_generator').async_generator
 
-async def print_yielded_values():
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    print(result)
+measure_runtime = __import__('2-measure_runtime').measure_runtime
 
-asyncio.run(print_yielded_values())
+
+async def main():
+    return await(measure_runtime())
+
+print(
+    asyncio.run(main())
+)
